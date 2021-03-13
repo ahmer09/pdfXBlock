f9f2dd4cb2832fbfe218d02e55160cfc4daf86bf
Ahmer Iqbal <ahmeriq09@hotmail.com>
2021-03-13 17:41:55 +0530

    update xbock.fragment to web_fragments

--- pdf/pdf.py
+++ pdf/pdf.py
@@ -5,7 +5,7 @@ from django.template import Context, Template
 
 from xblock.core import XBlock
 from xblock.fields import Scope, Integer, String, Boolean
-from xblock.fragment import Fragment
+from web_fragments.fragment import Fragment
 
 class pdfXBlock(XBlock):
 
